c6b12e0f78a0d1e5826ded6dd6afb4ae74962eeb
shivaram <shivarammittakola@gmail.com>
2024-01-13 20:22:04 +0530

    Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import hod from "../../assets/hod.png";
 import Slide from "./Home-Children/slides.jsx";
 import Info from "./Home-Children/info.jsx";
 import Events from "./Home-Children/events.jsx";
+import Numbers from "./Home-Children/numbers.jsx";
 
 export default function Home() {
 
@@ -13,6 +14,7 @@ export default function Home() {
            <Slide/>
                 <Info/>
                 <Events/>
+                <Numbers/>
             
         </>
     )
