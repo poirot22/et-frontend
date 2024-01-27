6c8d0302a0cc7b0b991a05641fb66c1b1c97b299
shivaram <shivarammittakola@gmail.com>
2024-01-27 09:37:23 +0530

    third

--- src/components/Home/Home-Children/numbers.jsx
+++ src/components/Home/Home-Children/numbers.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Image from '../../../assets/ByNumbers.jpg';
 import Sprite from '../../../assets/sprite.png';
+import cvrbackground from '../../../assets/cvrbackground.jpeg'
 import './numbers.css';
 export default function numbers() {
   return (
-    <div class="container bg-[url('C:/Users/SHIVARAM/ETReact/et-frontend/src/assets/ByNumbers.jpg')]">
-        <div class="grid grid-cols-4">
-            <span class="Sprite icon__ug bg-[url('C:\Users\SHIVARAM\ETReact\et-frontend\src\assets\sprite.png')]"></span>
+    <div class="container bg-[url('C:/Users/SHIVARAM/ETReact/et-frontend/src/assets/ByNumbers.jpg')] ">
+      <div class="grid grid-cols-3">
+        <div class="grid-cols-">
+          <div class="grid-cols-3">
+              <span class="Sprite icon__ug bg-[url('C:\Users\SHIVARAM\ETReact\et-frontend\src\assets\sprite.png')]"></span>
+          </div>
+          
         </div>
-    </div>
+
+      </div>
+    </div>
+    
   )
 }
