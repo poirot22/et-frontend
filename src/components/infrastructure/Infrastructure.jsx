e2df4a2829590ce53528790d1d141994a41c9a85
Gubbala Sai Nikhil <gsn.agc5022@gmail.com>
2024-01-28 10:48:04 +0530

    changed club

--- src/components/infrastructure/Infrastructure.jsx
+++ src/components/infrastructure/Infrastructure.jsx
@@ -1,18 +1,5 @@
-import React, { useState } from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import facultyCoordinator from "../../assets/facultycoordinators.png";
-import core from "../../assets/core.jpeg";
-import { Carousel } from "react-carousel-minimal";
-import event1 from "../../assets/event1.webp";
-import event2 from "../../assets/event2.jpeg";
-import event3 from "../../assets/event3.jpeg";
-import event4 from "../../assets/event4.jpeg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
 
 const Infrastructure = () => {
   return (
