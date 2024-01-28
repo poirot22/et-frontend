893a7be2a127f96026e8a8009c26f6b64e17e20e
Tejas <saitejas222@gmail.com>
2024-01-28 17:07:08 +0530

    added profile

--- src/components/Faculty/faculty.jsx
+++ src/components/Faculty/faculty.jsx
@@ -5,27 +5,85 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Modal = ({ selectedFaculty, onClose }) => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white rounded-lg">
-        <div class="flex justify-end mt-2 mr-3">
+    <div className="fixed top-0 left-0 w-full h-full  bg-black bg-opacity-50 flex items-center justify-center overflow-auto ">
+      <div className="bg-white rounded-lg w-3/4 shadow-lg">
+        <div className="flex justify-end mt-4 mr-4">
           <FontAwesomeIcon
             icon={faTimes}
-            class="transform transform-origin-center transition duration-300 ease-in-out hover:rotate-90 w-5 mt-1 mr-1"
+            className="transform transform-origin-center transition duration-300 ease-in-out hover:rotate-90 w-6 h-6 cursor-pointer text-gray-600"
             onClick={onClose}
           />
         </div>
 
-        <div class="pl-4 pr-4 pb-4">
-          <h1 className="text-lg font-bold mb-2">{selectedFaculty.id}</h1>
-          <h1 className="text-sm mb-2">
-            {selectedFaculty.lastName + " " + selectedFaculty.firstName}
+        <div className="p-6">
+          <div className="flex items-center justify-center mb-4">
+            <img
+              src={selectedFaculty.picture}
+              alt=""
+              className="h-32 w-32 rounded-md object-cover"
+            />
+          </div>
+          <h1 className="text-2xl font-bold mb-2 text-blue-600">
+            {selectedFaculty.id}
           </h1>
-          <h2 className="text-xs text-gray-600 mb-2">
-            {selectedFaculty.designation}
+          <h2 className="text-sm text-gray-600 mb-4">
+            {`${selectedFaculty.lastName} ${selectedFaculty.firstName}`}
           </h2>
-          <h3 className="text-xs text-gray-500 mb-2">
-            {selectedFaculty.email}
-          </h3>
+
+          <div className="grid grid-cols-2 gap-2 mb-4">
+            <div>
+              <h3 className="text-xs text-gray-500 mb-1">Designation:</h3>
+              <p className="text-sm">{selectedFaculty.designation}</p>
+            </div>
+            <div>
+              <h3 className="text-xs text-gray-500 mb-1">Email:</h3>
+              <p className="text-sm">{selectedFaculty.email}</p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-2 mb-4">
+            <div>
+              <h3 className="text-xs text-gray-500 mb-1">Specialization:</h3>
+              <p className="text-sm">{selectedFaculty.specialization}</p>
+            </div>
+            {selectedFaculty.joining_date && (
+              <div>
+                <h3 className="text-xs text-gray-500 mb-1">Joining Date:</h3>
+                <p className="text-sm">
+                  {selectedFaculty.joining_date.substring(0, 10)}
+                </p>
+              </div>
+            )}
+          </div>
+
+          <h3 className="text-xs text-gray-500 mb-1">Education:</h3>
+          <p className="text-sm mb-4">{selectedFaculty.education}</p>
+
+          <h3 className="text-xs text-gray-500 mb-1">Publications:</h3>
+          <ul className="list-disc pl-4 text-sm mb-4">
+            {selectedFaculty.publications.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+
+          <div className="grid grid-cols-2 gap-2">
+            <div>
+              <h3 className="text-xs text-gray-500 mb-1">Branch:</h3>
+              <p className="text-sm">{selectedFaculty.branch}</p>
+            </div>
+            <div>
+              <h3 className="text-xs text-gray-500 mb-1">
+                Publications Number:
+              </h3>
+              <p className="text-sm">{selectedFaculty.publications_number}</p>
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="text-xs text-gray-500 mb-1">Projects Guided:</h3>
+            <p className="text-sm">{selectedFaculty.projects_guided}</p>
+          </div>
+     
         </div>
       </div>
     </div>
